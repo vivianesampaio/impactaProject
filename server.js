4ca01341f841b505a59d2fa7a156aa6cebc44498
Viviane Sampaio <vivianesamsa@gmail.com>
2024-09-13 20:12:38 -0300

    databse integration

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors'; // Importa o middleware CORS
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors()); // Permite todas as origens (não recomendado para produção)
@@ -11,9 +15,15 @@ app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     console.log(req.body); // Loga o corpo da requisição
     const newUser = req.body;
+    
+    await prisma.task.create( { 
+        data: {
+            taskName: req.body.name,
+        }
+    } ) // Cria um novo usuário no banco de dados
 
     // Verifica se o nome está presente
     if (!newUser || !newUser.name) {
